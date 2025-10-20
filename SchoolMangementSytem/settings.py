3e23279e3feb380be21d6329919ab7171ed44a3c
zahra-waheed53 <zahra.waheed53@email.com>
2025-10-20 12:08:15 +0500

    AWS s3 bucket config for user images

--- SchoolMangementSytem/settings.py
+++ SchoolMangementSytem/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
-
+    'storages',  # For S3 storage
 
     'rest_framework',
     'rest_framework_simplejwt',
@@ -223,3 +223,18 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': crontab(hour='12-18', minute=0),
     },
 }
+
+# AWS S3 Configuration
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
+AWS_S3_REGION_NAME = config('AWS_S3_REGION_NAME', default='us-east-1')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+MEDIA_ROOT = 'media/'
--- users/models.py
+++ users/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+def user_image_upload_path(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f"{uuid.uuid4()}.{ext}"
+    return f"media/user_images/{filename}"
 
 
 class User(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=1000)
     phone = models.CharField(max_length=20)
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to=user_image_upload_path, null=True, blank=True)
     is_suspended = models.BooleanField(default=False)
     role = models.CharField(choices=[('admin', 'Admin'), ('student', 'Student'), ('teacher', 'Teacher'), ('staff', 'Staff')], max_length=20)
     classes = models.ForeignKey('api.Classes', on_delete=models.CASCADE, related_name="students", null=True, blank=True)
